8faafc1a8b9a2114f17b1bd422467e31c1727bb3
schadin <i@schadin.ru>
2026-08-14 17:56:45 +0300

    fix: прикрепление файлов и вставка изображений (TK-00025, TK-00026)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -53,6 +53,7 @@ pub fn run() {
         .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
         .manage(tray::DesktopState::default())
         .invoke_handler(tauri::generate_handler![
             tray::set_desktop_settings,
